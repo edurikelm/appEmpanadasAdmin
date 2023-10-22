ced014c4bdf639fe109b0f0d8e6855c58275f4a7
edurikelm <edurikelm.88@gmail.com>
2023-10-21 21:37:53 -0300

    Codigo para especificar carpeta publica en server

--- server/src/index.js
+++ server/src/index.js
@@ -1,18 +1,21 @@
 const express = require('express');
-const cors = require('cors')
-require('dotenv').config()
+const cors = require('cors');
+require('dotenv').config();
 const dbConnection = require('./config/database');
 
 const app = express();
 
 //Conectarse a BBDD
-dbConnection()
+dbConnection();
 
 //CORS
-app.use(cors())
+app.use(cors());
+
+//Directorio publico para visualizar nuestra pagina del fornt
+app.use(express.static('public'));
 
 //Lectura y parseo del body
-app.use(express.json())
+app.use(express.json());
 
 //Rutas
 app.use('/api/items', require('./routes/items'));
